e14dc5954d6ba141b81efe2cd5e4b7e65f14a5af
vnd_vunangdung <126397851+dungvnk1@users.noreply.github.com>
2024-03-16 20:11:00 +0700

    Update with app checking!
    Not work really well as expect, still check all audio in the whole system!

--- AudioTrackingApplication/MainWindow.xaml.cs
+++ AudioTrackingApplication/MainWindow.xaml.cs
@@ -1,65 +1,91 @@
-﻿using CSCore.CoreAudioAPI;
-using System.Text;
+﻿using NAudio.CoreAudioApi;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace AudioTrackingApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private MMDeviceEnumerator _deviceEnumerator;
-        private MMDevice _device;
-        private AudioMeterInformation _audioMeterInformation;
-        private DispatcherTimer dispatcherTimer;
+        private readonly MMDeviceEnumerator _deviceEnumerator;
+        private readonly MMDevice _device;
+        private readonly AudioMeterInformation _audioMeterInformation;
+        private DispatcherTimer _dispatcherTimer;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            
             _deviceEnumerator = new MMDeviceEnumerator();
             _device = _deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            _audioMeterInformation = AudioMeterInformation.FromDevice(_device);
+            _audioMeterInformation = _device.AudioMeterInformation;
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            StartMonitoring();
-        }
-
-        public void StartMonitoring()
-        {
-            dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += CheckAudioVolume;
-            dispatcherTimer.Interval = new System.TimeSpan(0, 0, 0, 0, 100); // 100ms
-            dispatcherTimer.Start();
-        }
-
-        private void CheckAudioVolume(object sender, System.EventArgs e)
-        {
-            // Read the current peak value
-            float peakValue = _audioMeterInformation.PeakValue;
-
-            // Check if the peak value is greater than 10%
-            if (peakValue > 0.1)
+            string appName = appNameTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(appName))
             {
-                // Since we're updating the UI, we need to make sure we're on the UI thread
+                RunApplication(appName);
+                StartMonitoring(appName);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the app name or path.");
+            }
+        }
+
+        private void RunApplication(string appPath)
+        {
+            try
+            {
+                // Starts the specified application and returns the process object associated with it.
+                Process.Start(appPath);
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                MessageBox.Show($"Could not start the application: {ex.Message}");
+            }
+        }
+
+        public void StartMonitoring(string appName)
+        {
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += (s, args) => CheckAudioVolume(appName);
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100); // 100ms
+            _dispatcherTimer.Start();
+        }
+
+        private void CheckAudioVolume(string appName)
+        {
+            var processes = Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(appName));
+            bool processFound = false;
+
+            foreach (var process in processes)
+            {
+                if (process.MainWindowHandle != IntPtr.Zero)
+                {
+                    processFound = true;
+                    float peakValue = _audioMeterInformation.MasterPeakValue;
+                    // Check if the peak value is greater than 10%
+                    if (peakValue > 0.1)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            MessageBox.Show($"Volume of {appName} is greater than 10%!");
+                        });
+                        _dispatcherTimer.Stop();
+                    }
+                    break;
+                }
+            }
+
+            if (!processFound)
+            {
                 Dispatcher.Invoke(() =>
                 {
-                    MessageBox.Show("Sound notice!");
+                    MessageBox.Show($"{appName} is not running or does not have a window.");
                 });
-                dispatcherTimer.Stop();
+                _dispatcherTimer.Stop();
             }
         }
     }
